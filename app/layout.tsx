2e7f89eb3a7fd7c27eab4697d97f6ca412d4218f
Nazar Koltsun <nazar-koltsun@ukr.net>
2023-08-17 16:59:43 +0300

    Added ToastProvider to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { ClerkProvider } from '@clerk/nextjs';
 import { ModalProvider } from '@/providers/modal-provider';
+import { ToastProvider } from '@/providers/toast-provider';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -20,8 +21,9 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
-          {children}
+          <ToastProvider />
           <ModalProvider />
+          {children}
         </body>
       </html>
     </ClerkProvider>
