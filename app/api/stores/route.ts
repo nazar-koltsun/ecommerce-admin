93e775bb7c2998ad0252befbe4994bc3b9aa14e2
Nazar Koltsun <nazar-koltsun@ukr.net>
2023-08-17 12:58:28 +0300

    Added 'create store' router

---
+++ app/api/stores/route.ts
@@ -0,0 +1,34 @@
+import prismadb from '@/lib/prismadb';
+import { auth } from '@clerk/nextjs';
+import { NextResponse } from 'next/server';
+
+export async function POST(reg: Request) {
+  try {
+    const { userId } = auth();
+    const body = await reg.json();
+
+    const { name } = body;
+
+    if (!name) {
+      return new NextResponse('Name is required', { status: 400 });
+    }
+    
+    if (!userId) {
+      return new NextResponse('Unauthorized', { status: 401 });
+    }
+
+    const store = prismadb.store.create({
+      data: {
+        name,
+        userId,
+      },
+    });
+
+    return NextResponse.json(store);
+
+  } catch (err) {
+    console.error('[STORES_POST]', err);
+    return new NextResponse('Internal Server Error', { status: 500 });
+  }
+}
+
