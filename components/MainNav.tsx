5542a5414d4fd4256835b51171bcfb8d547e7029
Nazar Koltsun <nazar-koltsun@ukr.net>
2023-08-21 11:51:33 +0300

    MainNav cmp

---
+++ components/MainNav.tsx
@@ -0,0 +1,39 @@
+'use client';
+
+import { cn } from '@/lib/utils';
+import Link from 'next/link';
+import { useParams, usePathname } from 'next/navigation';
+
+const MainNav = ({
+  className,
+  ...props
+}: React.HtmlHTMLAttributes<HTMLElement>) => {
+  const pathName = usePathname();
+  const params = useParams();
+
+  console.log(pathName, params);
+
+  const routes = [
+    {
+      href: `/${params.storeid}/settings`,
+      label: 'Settings',
+      active: pathName === `/${params.storeid}/settings`,
+    },
+  ];
+
+  return (
+    <nav className={cn('flex items-center space-x-4 ld:space-x-6', className)}>
+      {routes.map((route) => (
+        <Link 
+          key={route.href} 
+          href={route.href}
+          className={cn('text-sm font-medium transition-color hover:text-yellow-700', route.active && 'text-black dark:text-white')}
+        >
+          {route.label}
+        </Link>
+      ))}
+    </nav>
+  );
+};
+
+export default MainNav;
