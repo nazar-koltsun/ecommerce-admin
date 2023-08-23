f67dcc34f6c43da3b4600c6f77641bf5de57bc1e
Nazar Koltsun <nazar-koltsun@ukr.net>
2023-08-23 12:47:52 +0300

    Minor fix

--- providers/modal-provider.tsx
+++ providers/modal-provider.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useEffect, useState } from "react";
-import StoreModal from "@/components/modals/store-modal";
+import StoreModal from "@/components/modals/StoreModal";
 
 export const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
