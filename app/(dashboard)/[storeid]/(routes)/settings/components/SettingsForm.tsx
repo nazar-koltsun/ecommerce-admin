ada8b4ef438da558461fb45beb695713b74e7935
Nazar Koltsun <nazar-koltsun@ukr.net>
2023-08-23 15:34:50 +0300

    Settings page

--- app/(dashboard)/[storeid]/(routes)/settings/components/SettingsForm.tsx
+++ app/(dashboard)/[storeid]/(routes)/settings/components/SettingsForm.tsx
@@ -1,17 +1,89 @@
 'use client';
 
+import AlertModal from '@/components/modals/AlertModal';
 import { Button } from '@/components/ui/button';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
 import Heading from '@/components/ui/heading';
+import { Input } from '@/components/ui/input';
+import { Separator } from '@/components/ui/separator';
+import { zodResolver } from '@hookform/resolvers/zod';
 import { store as Store } from '@prisma/client';
+import axios from 'axios';
 import { Trash } from 'lucide-react';
+import { useParams, useRouter } from 'next/navigation';
+import { use, useState } from 'react';
+import { set, useForm } from 'react-hook-form';
+import { toast } from 'react-hot-toast';
+import * as zood from 'zod';
 
 interface SettingsFormProps {
   initialData: Store;
 }
 
+const formSchema = zood.object({
+  name: zood.string().min(1),
+});
+
+type SettingFormValues = zood.infer<typeof formSchema>;
+
 const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
+  const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const params = useParams();
+  const router = useRouter();
+
+  const form = useForm<SettingFormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialData,
+  });
+
+  const onSubmit = async (values: SettingFormValues) => {
+    try {
+      setLoading(true);
+      await axios.patch(`/api/stores/${params.storeid}`, values);
+      router.refresh();
+      toast.success('Store settings updated.');
+    } catch (error) {
+      toast.error('Something went wrong.');
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const deleteStore = async () => {
+    try {
+      setOpen(true);
+      setLoading(true);
+      await axios.delete(`/api/stores/${params.storeid}`);
+      router.refresh();
+      router.push('/');
+      toast.success('Store deleted.');
+    } 
+    catch (error) {
+      toast.error('Make sute that you are deleted all products and categories.');
+    }
+    finally {
+      setLoading(false);
+      setOpen(false);
+    }
+
+  };
+
   return (
     <>
+      <AlertModal
+        isOpen={open}
+        onClose={() => setOpen(false)}
+        onConfirm={deleteStore}
+        loading={loading}
+      />
       <div className="flex items-center justify-between">
         <Heading
           title="Settings"
@@ -20,12 +92,42 @@ const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
         <Button
           variant="destructive"
           size="icon"
-          onClick={() => console.log('hi')}
+          disabled={loading}
+          onClick={() => setOpen(true)}
         >
           <Trash className="w-4 h-4" />
         </Button>
       </div>
-      {/* <CommandSeparator /> */}
+      <Separator />
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-8 w-full"
+        >
+          <div className="grid grid-cols-3 gap-8">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Store name</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Store name"
+                      {...field}
+                      disabled={loading}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <Button disabled={loading} type="submit">
+            Save changes
+          </Button>
+        </form>
+      </Form>
     </>
   );
 };
