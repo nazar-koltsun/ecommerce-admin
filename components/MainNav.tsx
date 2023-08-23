fa4addc25d2f64ea3e15d5a086ac56a3670e6509
Nazar Koltsun <nazar-koltsun@ukr.net>
2023-08-23 15:33:50 +0300

    Added 'Overview' link

--- components/MainNav.tsx
+++ components/MainNav.tsx
@@ -15,6 +15,11 @@ const MainNav = ({
 
   const routes = [
     {
+      href: `/${params.storeid}`,
+      label: 'Overview',
+      active: pathName === `/${params.storeid}`,
+    },
+    {
       href: `/${params.storeid}/settings`,
       label: 'Settings',
       active: pathName === `/${params.storeid}/settings`,
