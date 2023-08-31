3a90698e219c67c408bb13f2eaa7f1e61590fee4
Nazar Koltsun <nazar-koltsun@ukr.net>
2023-08-31 15:48:40 +0300

    Updated Create billboard api route.

--- app/api/[storeId]/billboards/route.tsx
+++ app/api/[storeId]/billboards/route.tsx
@@ -16,7 +16,7 @@ export async function POST(
     const { label, imageUrl } = body;
 
     if (!userId) {
-      return new NextResponse('Unauthorized', { status: 401 });
+      return new NextResponse('Unauthentivated', { status: 401 });
     }
 
     if (!label) {
@@ -31,7 +31,18 @@ export async function POST(
       return new NextResponse('Store ID is required', { status: 400 });
     }
 
-    const resp = await prismadb.billboard.create({
+    const storeByUserId = await prismadb.store.findFirst({
+      where: {
+        id: storeId,
+        userId,
+      },
+    });
+
+    if(!storeByUserId) {
+      return new NextResponse('Unauthorised', { status: 403 });
+    }
+
+    const billboard = await prismadb.billboard.create({
       data: {
         storeId,
         label,
@@ -39,7 +50,7 @@ export async function POST(
       },
     });
 
-    return NextResponse.json(resp);
+    return NextResponse.json(billboard);
   } catch (err) {
     return new NextResponse('Internal Server Error', { status: 500 });
   }
