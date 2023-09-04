00d6af42ac12a0199557de638a2ab7e153273963
Nazar Koltsun <nazar-koltsun@ukr.net>
2023-09-04 16:47:56 +0300

    Billboards page fixes

--- app/(dashboard)/[storeid]/(routes)/billboards/page.tsx
+++ app/(dashboard)/[storeid]/(routes)/billboards/page.tsx
@@ -1,5 +1,7 @@
 import prismadb from "@/lib/prismadb";
 import BillboardClient from "./components/BillboardClient";
+import { BillboardColumn } from "./components/Columns";
+import { format } from 'date-fns'
 
 const BillboardsPage = async ({params}: {params: {storeId: string}}) => {
   const { storeId } = params;
@@ -13,10 +15,16 @@ const BillboardsPage = async ({params}: {params: {storeId: string}}) => {
     },
   })
 
+  const formatedBillboards: BillboardColumn[] = billboards.map(billboard => ({
+    id: billboard.id,
+    label: billboard.label,
+    createdAt: format (billboard.createdAt, 'MMMM do, yyyy'),
+  }));
+
   return (
     <div className="flex flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
-        <BillboardClient billboards={billboards} />
+        <BillboardClient billboards={formatedBillboards} />
       </div>
     </div>
   );
