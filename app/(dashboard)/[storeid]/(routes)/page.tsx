7ce5abfd62d39657978b71fb8e586fa32d1f57f2
Nazar Koltsun <nazar-koltsun@ukr.net>
2024-01-09 18:02:42 +0200

    Added cards to dashboard

--- app/(dashboard)/[storeid]/(routes)/page.tsx
+++ app/(dashboard)/[storeid]/(routes)/page.tsx
@@ -1,19 +1,63 @@
-import prismadb from "@/lib/prismadb";
+import Heading from '@/components/ui/heading';
+import { Separator } from '@/components/ui/separator';
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+
+import { formater } from '@/lib/utils';
+import { CreditCard, DollarSign, Package } from 'lucide-react';
 
 interface DashboardPageProps {
   params: {
     storeid: string;
-  }
+  };
 }
 
-const DashboardPage: React.FC<DashboardPageProps> = async ({params}) => {
-  const store = await prismadb.store.findFirst({
-    where: {
-      id: params.storeid
-    }
-  });
+const DashboardPage: React.FC<DashboardPageProps> = async ({ params }) => (
+  <div className="p-8 pt-6">
+    <Heading title="Dashboard" description="Overview of your store" />
+    <Separator />
+    <div className="mt-8 grid gap-4 grid-cols-3">
+      <Card>
+        <CardHeader>
+          <CardTitle className="flex gap-4 justify-between items-center text-sm">
+            Total revenue
+            <DollarSign size={15} />
+          </CardTitle>
+        </CardHeader>
+        <CardContent>
+          <p className="font-bold">{formater.format(100)}</p>
+        </CardContent>
+      </Card>
 
-  return <div>Active store: {store?.name}</div>;
-}
+      <Card>
+        <CardHeader>
+          <CardTitle className="flex gap-4 justify-between items-center text-sm">
+            Sales
+            <CreditCard size={15} />
+          </CardTitle>
+        </CardHeader>
+        <CardContent>
+          <p className="font-bold">+25</p>
+        </CardContent>
+      </Card>
+
+      <Card>
+        <CardHeader>
+          <CardTitle className="flex gap-4 justify-between items-center text-sm">
+            Total in Stock
+            <Package size={15} />
+          </CardTitle>
+        </CardHeader>
+        <CardContent>
+          <p className="font-bold">12</p>
+        </CardContent>
+      </Card>
+    </div>
+  </div>
+);
 
 export default DashboardPage;
